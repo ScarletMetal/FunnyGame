84bdb93315e76ae7ab54d430b0c14414a3ef1c94
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-06-23 09:14:07 +0000

    added basic player class

---
+++ player.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 40
+        self.height = 25
+        self.color = (66, 244, 161)
+
+    def update(self):
+        pass
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+
+    def change_x(self, x):
+        self.x += x
+
+    def change_y(self, y):
+        self.y += y
