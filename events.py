e68f4af9b03dd98b2bd83438fbc056b5f0a8eadb
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-06-23 12:21:53 +0000

    added enemy and player hit events

--- events.py
+++ events.py
@@ -5,8 +5,10 @@ class Events:
     def __init__(self):
         self.PLAYER_CHANGE_POS = "plcs"
         self.PLAYER_APPEND_BULLET = "plappblt"
+        self.PLAYER_HIT = "plhit"
 
         self.ENEMY_UPDATE_LOCATION = "enuploc"
+        self.ENEMY_HIT = "enhit"
 
         self.DRAW_GAME = "drgm"
         self.UPDATE_GAME = "updtgm"
