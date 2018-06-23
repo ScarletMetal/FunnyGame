4853c29d6c3e68d015dcc28186bb7549048f2b5d
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-06-23 13:11:42 +0000

    fixed event names

--- events.py
+++ events.py
@@ -3,15 +3,15 @@ from enum import Enum
 
 class Events:
     def __init__(self):
-        self.PLAYER_CHANGE_POS = "plcs"
-        self.PLAYER_APPEND_BULLET = "plappblt"
-        self.PLAYER_HIT = "plhit"
+        self.PLAYER_CHANGE_POS = "player-change-pos"
+        self.PLAYER_APPEND_BULLET = "player-append-bullet"
+        self.PLAYER_HIT = "player-hit"
 
-        self.ENEMY_UPDATE_LOCATION = "enuploc"
-        self.ENEMY_HIT = "enhit"
+        self.ENEMY_UPDATE_LOCATION = "enemy-update-location"
+        self.ENEMY_HIT = "enemy-hit"
 
-        self.DRAW_GAME = "drgm"
-        self.UPDATE_GAME = "updtgm"
+        self.DRAW_GAME = "draw-game"
+        self.UPDATE_GAME = "update-game"
 
 
 events = Events()
