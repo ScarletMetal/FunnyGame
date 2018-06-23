5c530ea41b2658f79dac391c56bf68a77c06e798
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-06-23 09:21:29 +0000

    added update_io function

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 import pygame
 from event_dispatcher import EventDispatcher
+from player import Player
+from events import Events
+
 dispatcher = EventDispatcher()
 pygame.init()
-
-
 run = True
 win = pygame.display.set_mode((400, 255))
 pygame.display.set_caption("Super Game")
+
+player = Player()
+
+
+def update_io(k):
+    if k[pygame.K_LEFT]:
+        dispatcher.dispatch(Events.PLAYER_CHANGE_X, x=-1)
+    if k[pygame.K_RIGHT]:
+        dispatcher.dispatch(Events.PLAYER_CHANGE_X, x=1)
+
+    if k[pygame.K_UP]:
+        dispatcher.dispatch(Events.PLAYER_CHANGE_Y, y=-1)
+    if k[pygame.K_DOWN]:
+        dispatcher.dispatch(Events.PLAYER_CHANGE_Y, y=1)
+
+
 while run:
     pygame.time.delay(100)
 
@@ -14,5 +31,7 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    keys = pygame.key.get_pressed()
+    update_io(keys)
 
 pygame.quit()
