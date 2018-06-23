d6de9a5c214c8bd61ee31beb806a68085f37f4bb
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-06-23 12:13:15 +0000

    made enemy class subscribe to events

--- enemy.py
+++ enemy.py
@@ -11,6 +11,9 @@ class Enemy:
         self.height = 25
         self.color = (244, 66, 232)
 
+        dispatcher.subscribe(Events.DRAW_GAME, self.draw)
+        dispatcher.subscribe(Events.ENEMY_UPDATE_LOCATION, self.update_location)
+
     def update_location(self, x, y):
         self.x = x
         self.y = y
