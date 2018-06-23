597807a4c1df0b9eebb50efff94bafe35d25217b
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-06-23 08:31:54 +0000

    added event_dispatcher class

---
+++ event_dispatcher.py
@@ -0,0 +1,13 @@
+class EventDispatcher:
+    def __init__(self):
+        self.events = {}
+
+    def subscribe(self, name, callback):
+        if self.events[name] is None:
+            self.events[name] = []
+        self.events[name].append(callback)
+
+    def dispatch(self, name):
+        if self.events[name] is not None:
+            for callback in self.events[name]:
+                callback()
