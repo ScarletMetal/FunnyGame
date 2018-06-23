69ed64a067973fe99a3d8d9e9074595665df7828
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-06-23 09:06:52 +0300

    Added mul definitions.

--- vector_generator.py
+++ vector_generator.py
@@ -1,8 +1,12 @@
-class Vector():
+class Vector:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def __mul__(self, other):
+        self.x *= other
+        self.y *= other
+
+    def __rmul__(self, other):
         self.x *= other
         self.y *= other
