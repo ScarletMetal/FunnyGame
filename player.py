5b5788998eab9a9551e5b5c80352b3f78a43c484
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-06-23 11:04:34 +0300

    Added force_inbounds.

--- player.py
+++ player.py
@@ -26,12 +26,24 @@ class Player:
         if direction_change != Vector(1, 1):
             self.velocity.x *= 0.5 * direction_change.x
             self.velocity.y *= 0.5 * direction_change.y
+            self.force_inbounds()
 
     def draw(self, win):
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
 
     def update_pos(self, vector):
         self.velocity += vector
+
+    def force_inbounds(self):
+        if 0 > self.x:
+            self.x = 0
+        elif self.x > constants.screen_width - self.width:
+            self.x = constants.screen_width - self.width
+
+        if 0 > self.y:
+            self.y = 0
+        elif self.y > constants.screen_height - self.height:
+            self.y = constants.screen_height - self.height
 
     def wall_detection(self):
         collision = Vector(1, 1)
