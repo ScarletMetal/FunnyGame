9b6931398540a10610fbd888a8342a998fdc6472
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-06-23 11:03:54 +0000

    added eq and ne to vector

--- vector_generator.py
+++ vector_generator.py
@@ -11,3 +11,9 @@ class Vector:
 
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
