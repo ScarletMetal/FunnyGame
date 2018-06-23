01c696cfe1d69629f85e303e22ff158fe20f4413
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-06-23 11:04:31 +0000

    fixed collision and update functions

--- player.py
+++ player.py
@@ -5,20 +5,27 @@ from vector_generator import Vector
 import constants
 
 
-
 class Player:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
+    def __init__(self, x=float(41), y=float(26)):
+        self.x = float(x)
+        self.y = float(y)
         self.width = 40
         self.height = 25
         self.color = (66, 244, 161)
-        self.velocity = Vector()
+        self.velocity = Vector(1, 1)
         dispatcher.subscribe(Events.PLAYER_CHANGE_POS, self.update_pos)
         dispatcher.subscribe(Events.DRAW_GAME, self.draw)
+        dispatcher.subscribe(Events.UPDATE_GAME, self.update)
 
     def update(self):
-        pass
+        # print(self.x)
+        print(self.velocity.x)
+        self.x += self.velocity.x
+        self.y += self.velocity.y
+        direction_change = self.wall_detection()  # collision physics
+        if direction_change != Vector(1, 1):
+            self.velocity.x *= 0.5 * direction_change.x
+            self.velocity.y *= 0.5 * direction_change.y
 
     def draw(self, win):
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
@@ -26,17 +33,12 @@ class Player:
     def update_pos(self, vector):
         self.velocity += vector
 
-        direction_change = self.wall_detection() #collision physics
-        if direction_change != Vector(1, 1):
-            self.velocity.x *= 0.5 * direction_change.x
-            self.velocity.y *= 0.5 * direction_change.y
-
     def wall_detection(self):
         collision = Vector(1, 1)
 
-        if not self.width < self.x < constants.screen_width - self.width:
+        if not 0 < self.x < constants.screen_width - self.width:
             collision.x = -1
-        if not self.height < self.y < constants.screen_height - self.height:
+        if not 0 < self.y < constants.screen_height - self.height:
             collision.y = -1
 
         return collision
