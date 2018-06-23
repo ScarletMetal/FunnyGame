591c5e1ba510c848f19cb57c4f27b610d0e7c4b0
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-06-23 11:30:13 +0000

    added cursor to the game

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from event_dispatcher import game_dispatcher as dispatcher
 from player import Player
 from events import events as Events
 from vector_generator import Vector
-
+from cursor import Cursor
 import constants
 
 pygame.init()
@@ -12,19 +12,20 @@ win = pygame.display.set_mode((constants.screen_width, constants.screen_height))
 pygame.display.set_caption("Super Game")
 
 player = Player()
+cursor = Cursor()
 clock = pygame.time.Clock()
 
 
 def update_player_velocity(keys):
     vector = Vector()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_a]:
         vector.x = -0.5
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_d]:
         vector.x = 0.5
 
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_w]:
         vector.y = -0.5
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_s]:
         vector.y = 0.5
 
     if vector != Vector():
