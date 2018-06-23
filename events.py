89b93cc7d968741e01745a92e23b40f57a76e176
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-06-23 11:59:40 +0000

    added PLAYER_APPEND_BULLET event

--- events.py
+++ events.py
@@ -4,9 +4,11 @@ from enum import Enum
 class Events:
     def __init__(self):
         self.PLAYER_CHANGE_POS = "plcs"
+        self.PLAYER_APPEND_BULLET = "plappblt"
 
         self.DRAW_GAME = "drgm"
         self.UPDATE_GAME = "updtgm"
+
 
 
 events = Events()
