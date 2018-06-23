2d06d09d44e8217eb039f9d48e09508797b20b0b
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-06-23 12:12:35 +0000

    added enemy_update_location event

--- events.py
+++ events.py
@@ -6,6 +6,8 @@ class Events:
         self.PLAYER_CHANGE_POS = "plcs"
         self.PLAYER_APPEND_BULLET = "plappblt"
 
+        self.ENEMY_UPDATE_LOCATION = "enuploc"
+
         self.DRAW_GAME = "drgm"
         self.UPDATE_GAME = "updtgm"
 
