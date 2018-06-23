04c3dc50c1df4505c55d3d01f55394b1eced1422
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-06-23 09:29:00 +0300

    Added default x,y values and changed "other" to "scalar"

--- vector_generator.py
+++ vector_generator.py
@@ -1,13 +1,13 @@
 class Vector:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = float(x)
         self.y = float(y)
 
-    def __mul__(self, other):
-        return Vector(self.x * other, self.y * other)
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
 
-    def __rmul__(self, other):
-        return Vector(self.x * other, self.y * other)
+    def __rmul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
 
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
