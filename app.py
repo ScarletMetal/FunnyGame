6a4b0d2450cbf97638987d61a1ee2efc63971da0
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-06-23 11:21:54 +0000

    added fps lock to the game

--- app.py
+++ app.py
@@ -12,19 +12,20 @@ win = pygame.display.set_mode((constants.screen_width, constants.screen_height))
 pygame.display.set_caption("Super Game")
 
 player = Player()
+clock = pygame.time.Clock()
 
 
 def update_player_velocity(keys):
     vector = Vector()
     if keys[pygame.K_LEFT]:
-        vector.x = -0.1
+        vector.x = -0.5
     if keys[pygame.K_RIGHT]:
-        vector.x = 0.1
+        vector.x = 0.5
 
     if keys[pygame.K_UP]:
-        vector.y = -0.1
+        vector.y = -0.5
     if keys[pygame.K_DOWN]:
-        vector.y = 0.1
+        vector.y = 0.5
 
     if vector != Vector():
         dispatcher.dispatch(Events.PLAYER_CHANGE_POS, vector=vector)
@@ -33,7 +34,9 @@ def update_player_velocity(keys):
 def update_io(keys):
     update_player_velocity(keys)
 
+
 while run:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
