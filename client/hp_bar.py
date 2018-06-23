8c3512ac1d2fe0c214e3700cabac02e37c6d7232
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-06-23 14:47:28 +0300

    Added hp bar class.

---
+++ client/hp_bar.py
@@ -0,0 +1,17 @@
+import pygame
+
+from client.event_dispatcher import game_dispatcher as dispatcher
+from client.events import events as Events
+
+
+class HP_Bar:
+    def __init__(self, player):
+        self.player = player
+        dispatcher.subscribe(Events.DRAW_GAME, self.draw)
+        dispatcher.subscribe(Events.UPDATE_GAME, self.update)
+
+    def update(self):
+        pass
+
+    def draw(self, win):
+        win.blit(pygame.font.SysFont('Comic Sans MS', 30).render(str(self.player.health)))
