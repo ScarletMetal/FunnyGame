07e43348f5a1c4d40110a0314ad013025d6ebb97
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-06-23 09:19:09 +0300

    Fixed mul definitions.

--- vector_generator.py
+++ vector_generator.py
@@ -1,12 +1,10 @@
 class Vector:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
 
     def __mul__(self, other):
-        self.x *= other
-        self.y *= other
+        return Vector(self.x * other, self.y * other)
 
     def __rmul__(self, other):
-        self.x *= other
-        self.y *= other
+        return Vector(self.x * other, self.y * other)
